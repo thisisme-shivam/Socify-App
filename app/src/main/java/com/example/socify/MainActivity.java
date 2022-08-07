ae03e562bc1b5e2b6363b27d6b33c153cb3b23f7
sangeetkarthecoder <bauddhsaurav@gmail.com>
2022-08-07 06:10:13 +0530

    "OnBoarding Screen Done, Testing on different devices left"

--- app/src/main/java/com/example/socify/MainActivity.java
+++ app/src/main/java/com/example/socify/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.socify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.cuberto.liquid_swipe.LiquidPager;
+import com.example.socify.databinding.ActivityMainBinding;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    LiquidPager liquidPager;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        liquidPager = binding.pager;
+        viewPager = new ViewPager(getSupportFragmentManager(), 1);
+        liquidPager.setAdapter(viewPager);
+        Objects.requireNonNull(getSupportActionBar()).hide();
     }
 }
