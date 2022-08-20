a29ade8f74f3054d78ff74bcc9061722dc222250
sangeetkarthecoder <bauddhsaurav@gmail.com>
2022-08-20 18:51:08 +0530

    Bottom Sheet Done

--- app/src/main/java/com/example/socify/HomeFragments/NewsFeedFragment.java
+++ app/src/main/java/com/example/socify/HomeFragments/NewsFeedFragment.java
@@ -1,16 +1,32 @@
 package com.example.socify.HomeFragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.socify.Activities.AllChat;
 import com.example.socify.R;
+import com.example.socify.databinding.FragmentNewsFeedBinding;
 
 public class NewsFeedFragment extends Fragment {
+
+    FragmentNewsFeedBinding binding;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.chat.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), AllChat.class);
+            startActivity(intent);
+        });
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +37,8 @@ public class NewsFeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_feed, container, false);
+
+        binding = FragmentNewsFeedBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }
