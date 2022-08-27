f051e67b5b35f997703039ba4458f269fb69b7a6
shivam kumar <kumarshi605@gmail.com>
2022-08-27 12:06:42 +0530

    chat activity

--- app/src/main/java/com/example/socify/Activities/VerificationActivity.java
+++ app/src/main/java/com/example/socify/Activities/VerificationActivity.java
@@ -4,48 +4,73 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.socify.R;
+import com.example.socify.databinding.ActivityVerificationBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class VerificationActivity extends AppCompatActivity {
 
-
-    Button btn_get_otp;
+    ActivityVerificationBinding binding;
+    Dialog otpDialog;
+    FirebaseAuth auth;
     ImageView close_dialog, back_btn;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_verification);
-
-        btn_get_otp = findViewById(R.id.getOtpButton);
-
-        Dialog otpDialog = new Dialog(VerificationActivity.this);
-        otpDialog.setCancelable(false);
-        otpDialog.setContentView(R.layout.otp_verification);
-        otpDialog.getWindow().setWindowAnimations(R.style.DialogAnimation);
-
+    private void setOnclickListners(){
         otpDialog.findViewById(R.id.otpSubmitButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(VerificationActivity.this, Registration.class));
                 finish();
-    }
-});
+            }
+        });
 
         close_dialog = otpDialog.findViewById(R.id.close_icon);
 
-        btn_get_otp.setOnClickListener(view -> otpDialog.show());
+        otpDialog.findViewById(R.id.otpInput).requestFocus();
+        binding.getOtpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                otpDialog.show();
+                otpDialog.findViewById(R.id.otpInput).requestFocus();
+                ((InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE)).toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
 
-        close_dialog.setOnClickListener(view -> otpDialog.dismiss());
+            }
+        });
+        close_dialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                otpDialog.dismiss();
+                binding.phoneInput.requestFocus();
+
+            }
+        });
+
+
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityVerificationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        otpDialog = new Dialog(VerificationActivity.this);
+        otpDialog.setCancelable(false);
+        otpDialog.setContentView(R.layout.otp_verification);
+        otpDialog.getWindow().setWindowAnimations(R.style.DialogAnimation);
+        setOnclickListners();
+
         
     }
 
