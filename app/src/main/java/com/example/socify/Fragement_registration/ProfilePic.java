85ca8552da57794689c1d57535ca9a1dd57b433d
shivam kumar <kumarshi605@gmail.com>
2022-08-28 20:20:50 +0530

    last all workinng

--- app/src/main/java/com/example/socify/Fragement_registration/ProfilePic.java
+++ app/src/main/java/com/example/socify/Fragement_registration/ProfilePic.java
@@ -1,19 +1,9 @@
 package com.example.socify.Fragement_registration;
 
-import android.app.Activity;
 import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,8 +11,13 @@ import android.view.ViewGroup;
 import android.webkit.MimeTypeMap;
 import android.widget.ProgressBar;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.example.socify.Activities.CropperActivity;
-import com.example.socify.Activities.Registration;
 import com.example.socify.R;
 import com.example.socify.databinding.FragmentProfilePicBinding;
 import com.google.firebase.firestore.DocumentReference;
@@ -30,9 +25,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.squareup.picasso.Picasso;
-
-import java.util.Objects;
 
 
 public class ProfilePic extends Fragment {
