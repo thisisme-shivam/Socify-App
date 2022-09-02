0bfc6eb1118a16895b7076aef31f8ea6b4f4a443
sangeetkarthecoder <bauddhsaurav@gmail.com>
2022-09-02 15:34:55 +0530

    saurav merged thread

---
+++ app/src/main/java/com/example/socify/Classes/QuestionsMember.java
@@ -0,0 +1,66 @@
+package com.example.socify.Classes;
+
+public class QuestionsMember {
+    String name,url,userid,key,question,time,tag;
+
+    public QuestionsMember() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
+
