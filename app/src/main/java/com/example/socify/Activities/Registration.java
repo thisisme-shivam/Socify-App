36266b07e77df59d5d110ff8545f684693b16ad4
sangeetkarthecoder <bauddhsaurav@gmail.com>
2022-09-04 16:46:29 +0530

    "Work Started on Query Replies"

--- app/src/main/java/com/example/socify/Activities/Registration.java
+++ app/src/main/java/com/example/socify/Activities/Registration.java
@@ -1,9 +1,7 @@
 package com.example.socify.Activities;
 
 import android.os.Bundle;
-import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.socify.Classes.College;
@@ -12,11 +10,7 @@ import com.example.socify.FireBaseClasses.UserDetails;
 import com.example.socify.R;
 import com.example.socify.RegistrationFragments.ProfilePic;
 import com.example.socify.databinding.ActivityRegistrationBinding;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Objects;
