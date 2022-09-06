9c5baa063bbbde0a719185c511b86fc01c467c24
sangeetkarthecoder <bauddhsaurav@gmail.com>
2022-09-06 19:25:59 +0530

    "Work on QueryHub Almost Finished"

--- app/src/main/java/com/example/socify/QueryFragments/UserQueriesFragment.java
+++ app/src/main/java/com/example/socify/QueryFragments/UserQueriesFragment.java
@@ -8,33 +8,75 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.socify.Classes.QuestionsMember;
 import com.example.socify.R;
 import com.example.socify.ViewHolders.Load_Questions;
-import com.example.socify.databinding.FragmentQueryListBinding;
 import com.example.socify.databinding.FragmentUserQueriesBinding;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserQueriesFragment extends Fragment {
 
     FragmentUserQueriesBinding binding;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference databaseReference;
+    DatabaseReference databaseReference = database.getReference("Questions").child("User's Questions").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+
+    //Deleting User's Questions from the database when delete button is tapped
+    public void delete(String time) {
+
+        //Deleting from 'User's Questions' node
+        Query query = databaseReference.orderByChild("time").equalTo(time);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                    dataSnapshot.getRef().removeValue();
+
+                    Toast.makeText(requireActivity(), "Deleted", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //Deleting from 'All Questions' node
+        databaseReference = database.getReference("Questions").child("All Questions");
+        Query query1 = databaseReference.orderByChild("time").equalTo(time);
+        query1.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                    dataSnapshot.getRef().removeValue();
+                    Toast.makeText(requireActivity(), "Deleted", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+        });
+
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-
     }
 
     @Override
@@ -45,43 +87,36 @@ public class UserQueriesFragment extends Fragment {
 
 
         //Creating Adapter to Load the data in the RecyclerView
-        databaseReference = database.getReference("Questions").child("All Questions");
+        databaseReference = database.getReference("Questions").child("User's Questions").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
         FirebaseRecyclerOptions<QuestionsMember> options =
                 new FirebaseRecyclerOptions.Builder<QuestionsMember>()
                         .setQuery(databaseReference,QuestionsMember.class)
                         .build();
 
+
         FirebaseRecyclerAdapter<QuestionsMember, Load_Questions> firebaseRecyclerAdapter =
                 new FirebaseRecyclerAdapter<QuestionsMember, Load_Questions>(options) {
                     @Override
                     protected void onBindViewHolder(@NonNull Load_Questions holder, int position, @NonNull QuestionsMember model) {
-                        holder.setitem(getActivity(), model.getName(),model.getUrl(),model.getUserid(),model.getKey(),model.getQuestion(),model.getTime(),model.getTag());
+
+                        holder.deleteitem(getActivity(), model.getName(),model.getUrl(),model.getUserid(),model.getKey(),model.getQuestion(),model.getTime(),model.getTag());
+                        final String time = getItem(position).getTime();
+                        holder.delbtn.setOnClickListener(v -> delete(time));
+
                     }
 
                     @NonNull
                     @Override
                     public Load_Questions onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
                         View view = LayoutInflater.from(parent.getContext())
-                                .inflate(R.layout.list_query_layout,parent,false);
+                                .inflate(R.layout.list_user_query_layout,parent,false);
                         return new Load_Questions(view);
                     }
                 };
 
         firebaseRecyclerAdapter.startListening();
         binding.userqueriesRV.setAdapter(firebaseRecyclerAdapter);
-
-
-
-
-
-
-
         return binding.getRoot();
-
-
-
-
-
 
     }
 }
