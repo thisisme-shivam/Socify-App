1b9c291c345ca85f67ff11b48bf01f25383b0717
iamryuga <kumarshi605@gmail.com>
2022-09-21 22:15:14 +0530

    naem

--- app/src/main/java/com/example/socify/fragment_profile1.java
+++ app/src/main/java/com/example/socify/fragment_profile1.java
@@ -59,6 +59,6 @@ public class fragment_profile1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile1, container, false);
+        return inflater.inflate(R.layout.fragment_visite_profile, container, false);
     }
 }
