5deda416492404cc720fb906c356e6f93fb5ae8e
iamryuga <kumarshi605@gmail.com>
2022-09-21 19:39:07 +0530

    visiting profile

---
+++ app/src/main/java/com/example/socify/friend_list.java
@@ -0,0 +1,14 @@
+package com.example.socify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class friend_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_list);
+    }
+}
